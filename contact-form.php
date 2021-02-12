bd1f32496ac790eec58690f035aafd13349203bf
Eternista <Eternista@users.noreply.github.com>
2021-02-12 10:59:12 +0100

    New Code

--- contact-form.php
+++ contact-form.php
@@ -1,19 +1,55 @@
 <?php
 
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+$errors = [];
+$errorMessage = '';
 
-$email_form = "skullsicked@gmail.com";
-$email_subject = "New Form Submision";
-$email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"User Message: $message.\n";
+$secret = 'your secret key';
 
-$to = "rafal.ziolkowski1234@gmail.com";
-$headers = "From: $email_form \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
 
-mail($to,$email_subject, $email_body, $headers);
+    $recaptchaUrl = "https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$recaptchaResponse}";
+    $verify = json_decode(file_get_contents($recaptchaUrl));
+
+    if (!$verify->success) {
+      $errors[] = 'Recaptcha failed';
+    }
+
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
+    }
+
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+
+
+    if (!empty($errors)) {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    } else {
+        $toEmail = 'example@example.com';
+        $emailSubject = 'New email from your contant form';
+        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+
+        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+        $body = join(PHP_EOL, $bodyParagraphs);
+
+        if (mail($toEmail, $emailSubject, $body, $headers)) {
+            header('Location: thank-you.html');
+        } else {
+            $errorMessage = "<p style='color: red;'>Oops, something went wrong. Please try again later</p>";
+        }
+    }
+}
 
 ?>
