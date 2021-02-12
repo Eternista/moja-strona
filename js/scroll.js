a0f9999fd3a423ec10c3c8fc3bacafb035747fbe
Eternista <Eternista@users.noreply.github.com>
2021-02-12 13:27:49 +0100

    Contact

--- js/scroll.js
+++ js/scroll.js
@@ -8,14 +8,15 @@ window.addEventListener('scroll', function(){
             followBox.classList.remove('active')
         }
     }
-    else if(window.scrollY > 200 & window.scrollY < 800) {
+    else if(window.scrollY > 200 & window.scrollY < 900) {
         followBox.classList.remove('active0');
         followBox.classList.add('active')
         followBox.classList.remove('active2')
-    } else if(window.scrollY > 800 && window.scrollY < 1300) {
+    } else if(window.scrollY > 900 && window.scrollY < 1500) {
         followBox.classList.remove('active')
         followBox.classList.add('active2')
-    }  else if(window.scrollY > 1300 && window.scrollY < 2400) {
+        followBox.classList.remove('active3')
+    }  else if(window.scrollY > 1500 && window.scrollY < 2500) {
         followBox.classList.remove('active2')
         followBox.classList.add('active3')
     }
